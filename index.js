bfa2e1fa7a5c8ca766b8a7be8d9abf444fa1d300
1010 <134185805+jedenzero@users.noreply.github.com>
2024-06-25 16:05:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const lang = new URLSearchParams(location.search).get('lang');
 var codes = [];
 var words = [];
 
-fetch(`https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVjQO8InAYk3g28NAEfU/values/codes!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
-.then(response=>response.json())
-.then(data=>{
+async function fetchData(){
+    const response=await fetch(`https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVjQO8InAYk3g28NAEfU/values/codes!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
+    const data=await response.json();
     if(lang){
         codes=data.values;
         words=await getByCode(lang);
@@ -12,13 +12,12 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVj
     else{
         
     }
-})
-.catch(error=>console.error('Error:',error));
+}
 
 async function getByCode(code){
     for(const row of codes){
         if(row[1]===code){
-            const reponse=await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${row[0]}/values/${row[1]}!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
+            const response=await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${row[0]}/values/${row[1]}!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
             const data=await response.json();
             return data.values;
         }
