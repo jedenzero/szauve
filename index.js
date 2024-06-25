d3ab5e630674546e6df6838ee1cca219b9a4337f
1010 <134185805+jedenzero@users.noreply.github.com>
2024-06-25 15:56:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const lang = new URLSearchParams(location.search).get('lang');
 var codes = [];
 var words = [];
 
-fetch('https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVjQO8InAYk3g28NAEfU/values/codes!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk')
+fetch(`https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVjQO8InAYk3g28NAEfU/values/codes!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
 .then(response=>response.json())
 .then(data=>{
     if(lang){
         codes=data.values;
-        words=getByCode(lang);
+        words=await getByCode(lang);
     }
     else{
         
@@ -15,15 +15,32 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/17ZVfLP8WImY1S--yA7gojHLiVj
 })
 .catch(error=>console.error('Error:',error));
 
-function getByCode(code){
-    codes.forEach(row=>{
+async function getByCode(code){
+    for(const row of codes){
         if(row[1]===code){
-            fetch(`https://sheets.googleapis.com/v4/spreadsheets/${row[0]}/values/${row[1]}!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
-            .then(response=>response.json())
-            .then(data=>{
-                return data.values;
-            })
-            .catch(error=>console.error('Error:',error));
+            const reponse=await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${row[0]}/values/${row[1]}!A:D?key=AIzaSyATLeHQh6kM0LWRJjLg8CmzoSdnntFrmFk`)
+            const data=await response.json();
+            return data.values;
         }
+    }
+}
+
+function search(){
+    document.querySelector('#output').innerHTML='';
+    const target=document.querySelector('input').value;
+    var targets=[...words.filter(row=>row[1].startsWith(target)),...words.filter(row=>!row[1].startsWith(target)&&row[1].includes(target))];
+    targets.forEach(row=>{
+        show(row);
     });
+}
+
+function show(word){
+    document.querySelector('#output').innerHTML+=`
+    <div class='word'>
+    <div>${word[1]}</div>
+    <div>${word[2]}</div>
+    <div>어원</div>
+    <div>기타 속성</div>
+    </div>
+    `;
 }
