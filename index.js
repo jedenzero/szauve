4a15ff54e3416efb203b6b35473d3d55e1800f24
1010 <134185805+jedenzero@users.noreply.github.com>
2024-07-18 14:19:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ function search(){
     document.querySelector('#output').innerHTML='';
     const target=document.querySelector('input').value;
     const startsWithTarget = words.filter(row =>
-          row[1].startsWith(target) || row[2].split(', ').some(el=>el.split(':')[1].startsWith(target)) || row[3].startsWith(target)
+          row[1].startsWith(target) || (row?.[2].split(', ').some(el=>el.split(':')[1].startsWith(target)) || '') || row[3].startsWith(target)
     );
     
     const containsTarget = words.filter(row =>
-      !startsWithTarget.includes(row) && (row[1].includes(target) || row[2].split(', ').some(el=>el.split(':')[1].includes(target)) || row[3].includes(target))
+      !startsWithTarget.includes(row) && (row[1].includes(target) || (row?.[2].split(', ').some(el=>el.split(':')[1].includes(target)) || '') || row[3].includes(target))
     );
     
     const targets = [...startsWithTarget, ...containsTarget];
