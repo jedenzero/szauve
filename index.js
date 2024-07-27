aa068e2c8097822020bd68ce257e2a78830f390b
1010 <134185805+jedenzero@users.noreply.github.com>
2024-07-27 22:28:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,42 +11,7 @@ async function fetchData(){
     if(lang){
         codes=data.values;
         words=await getByCode(lang);
-        // 특수문자 추리기
-        /**
-        for(const row of words){
-            if(!test.test(row[1])){
-                for(const el of row[1]){
-                    if(!test.test(el)&&!specials.includes(el)){
-                        specials.push(el);
-                    }
-                }
-            }
-        }
-        specials.sort((a,b)=>a.localeCompare(b));
-        specials.forEach(el=>{
-            document.querySelector('#special').innerHTML+=`<div onclick="inputSpecial(this);">${el}</div>`
-        });
-        **/
-        // 단어 수
-        document.querySelector('#words').innerHTML=`<div style="font-size:0.8rem;">단어</div>
-        <div style="text-align:center;">
-        <span style="font-size:2rem;font-weight:bold;">${words.length}</span><span style="font-size:0.8rem;">개</span>
-        </div>`;
-        // 가장 긴 단어
-        var long=['',''];
-        for(const row of words){
-            if(row[1].length>long[1].length){
-                long=row;
-            }
-        }
-        document.querySelector('#long').innerHTML=`<div>가장 긴 단어</div>
-        <div><sup>${long[0]}</sup><span>${long[1]}</span></div>
-        <div>${long[3].split(', ')[0]}</div>`;
-        // 무작위 단어
-        var random=words[Math.floor(words.length*Math.random())];
-        document.querySelector('#random').innerHTML=`<div>무작위 단어</div>
-        <div><sup>${random[0]}</sup><span>${random[1]}</span></div>
-        <div>${random[3].split(', ')[0]}</div>`;
+        setGrid();
     }
     else{
         
@@ -70,6 +35,58 @@ async function getByCode(code){
     }
 }
 
+function setGrid(){
+    // 특수문자 추리기
+    /**
+    for(const row of words){
+        if(!test.test(row[1])){
+            for(const el of row[1]){
+                if(!test.test(el)&&!specials.includes(el)){
+                    specials.push(el);
+                }
+            }
+        }
+    }
+    specials.sort((a,b)=>a.localeCompare(b));
+    specials.forEach(el=>{
+        document.querySelector('#special').innerHTML+=`<div onclick="inputSpecial(this);">${el}</div>`
+    });
+    **/
+    // 단어 수
+    document.querySelector('#words').innerHTML=`<div style="font-size:0.8rem;">단어</div>
+    <div style="text-align:center;">
+    <span style="font-size:2rem;font-weight:bold;">${words.length}</span><span style="font-size:0.8rem;">개</span>
+    </div>`;
+    var parts={};
+    var long=['',''];
+    for(const row of words){
+        //품사
+        if(!Object.keys(parts).includes(row[4])){
+            parts[row[4]]=1;
+        }
+        else{
+            parts[row[4]]++;
+        }
+        // 가장 긴 단어
+        if(row[1].length>long[1].length){
+            long=row;
+        }
+    }
+    document.querySelector('#words').innerHTML=`<div style="font-size:0.8rem;">품사</div>
+    <div style="text-align:center;">
+    <span style="font-size:2rem;font-weight:bold;">${Object.keys(parts).length}</span><span style="font-size:0.8rem;">개</span>
+    </div>`;
+    document.querySelector('#long').innerHTML=`<div>가장 긴 단어</div>
+    <div><sup>${long[0]}</sup><span>${long[1]}</span></div>
+    <div>${long[3].split(', ')[0]}</div>`;
+    // 무작위 단어
+    var random=words[Math.floor(words.length*Math.random())];
+    document.querySelector('#random').innerHTML=`<div>무작위 단어</div>
+    <div><sup>${random[0]}</sup><span>${random[1]}</span></div>
+    <div>${random[3].split(', ')[0]}</div>`;
+    // 그래프
+}
+
 function inputSpecial(letter){
     document.querySelector('input').value+=letter.textContent;
     document.querySelector('input').focus();
@@ -82,11 +99,13 @@ function search(){
     if(target===''){
         document.querySelector('#c1').style.display='grid';
         document.querySelector('#c2').style.display='grid';
+        document.querySelector('#graph').style.display='grid';
         document.querySelector('#output').style.display='none';
     }
     else{
         document.querySelector('#c1').style.display='none';
         document.querySelector('#c2').style.display='none';
+        document.querySelector('#graph').style.display='none';
         document.querySelector('#output').style.display='block';
         
         const startsWithTarget = words.filter(row =>
