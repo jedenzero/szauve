1f13a6598c1d221614420f513e83c88fbcb1cd41
1010 <134185805+jedenzero@users.noreply.github.com>
2024-10-20 01:20:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ async function start(){
       });
   }
   else{
-	  l = langs.find(l => l[0]==lang)[1];
+	  l = langs.find(l => l[0]==lang);
 	  
       input.style.display = 'block';
 	  title.style.display = 'block';
-	  title.innerHTML = `<b>${l}</b> <span>사전</span>`;
+	  title.innerHTML = `<b>${l[1]}</b> <span>사전</span>`;
       words = await getWords();
       if(word){
 		input.value = word;
